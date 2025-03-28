2d37ec12e41c7c8c6122748ca76a0b62c5f24887
kiuusai <kiuusai@protonmail.com>
2025-03-28 01:07:26 -0300

    Removing UTXO interface from index

--- packages/pls-bitcoin/index.ts
+++ packages/pls-bitcoin/index.ts
@@ -3,12 +3,6 @@ import { createBitcoinMultisig } from "./createBitcoinMultisig.js";
 import { startTxSpendingFromMultisig } from "./startTxSpendingFromMultisig.js";
 import { combine, H, bitcoinSchemas } from "./utils/index.js"
 
-export interface UTXO {
-	txid: string;
-	vout: number;
-	value: number;
-}
-
 export {
 	createKeyTweaker,
 
