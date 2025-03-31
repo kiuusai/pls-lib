4be957e363f04ace592188838f96b9a1c1b8e495
kiuusai <kiuusai@protonmail.com>
2025-03-31 18:41:00 -0300

    Adapting older tests to run with tweaking

--- packages/pls-liquid/index.test.ts
+++ packages/pls-liquid/index.test.ts
@@ -1,4 +1,4 @@
-import { afterEach, describe, expect, test } from "vitest";
+import { afterEach, describe, expect } from "vitest";
 import {
 	createLiquidMultisig,
 	finalizeTxSpendingFromLiquidMultisig,
@@ -7,30 +7,12 @@ import {
 	startSpendFromLiquidMultisig,
 } from "./index.js";
 import { ECPairFactory, ECPairInterface } from "ecpair";
-import { Pset, Transaction, address, bip341, networks } from "liquidjs-lib";
+import { Pset, Transaction, address, bip341, networks, crypto } from "liquidjs-lib";
 
 import * as ecc from "tiny-secp256k1";
+import { createKeyTweaker } from "pls-bitcoin/createKeyTweaker.js";
 
 const ECPair = ECPairFactory(ecc);
-
-const keypair1 = ECPair.fromPrivateKey(
-	Buffer.from(
-		"0000000000000000000000000000000000000000000000000000000000000001",
-		"hex"
-	)
-);
-const keypair2 = ECPair.fromPrivateKey(
-	Buffer.from(
-		"0000000000000000000000000000000000000000000000000000000000000002",
-		"hex"
-	)
-);
-const keypair3 = ECPair.fromPrivateKey(
-	Buffer.from(
-		"0000000000000000000000000000000000000000000000000000000000000003",
-		"hex"
-	)
-);
 
 const API_URL = "http://localhost:3001";
 
@@ -89,19 +71,31 @@ async function retryWithDelay<T extends any>(
 	throw new Error(`Failed after ${tries} retries`);
 }
 
-async function testMultisigWithParameters(
-	clientKeypairs: ECPairInterface[],
-	arbitratorKeypairs: ECPairInterface[],
-	quorum: number,
-	redeemOutput: string,
-	usedKeysCombination: ECPairInterface[]
-) {
-	const multisig = createLiquidMultisig(
-		clientKeypairs.map((ecpair) => ecpair.publicKey.toString("hex")),
-		arbitratorKeypairs.map((ecpair) => ecpair.publicKey.toString("hex")),
-		quorum,
-		networks.regtest
-	);
+type TestMultisigWithParametersArgs = {
+	clientKeypairs: ECPairInterface[];
+	arbitratorKeypairs: ECPairInterface[];
+	quorum: number;
+	usedKeysCombination: ECPairInterface[];
+	blindingKeypair: ECPairInterface;
+	tweak: Buffer;
+}
+
+async function testMultisigWithParameters({
+	clientKeypairs,
+	arbitratorKeypairs,
+	quorum,
+	usedKeysCombination,
+	blindingKeypair,
+	tweak,
+}: TestMultisigWithParametersArgs) {
+	const multisig = createLiquidMultisig({
+		parts: clientKeypairs.map((ecpair) => ecpair.publicKey.toString("hex")),
+		arbitrators: arbitratorKeypairs.map((ecpair) => ecpair.publicKey.toString("hex")),
+		arbitratorsQuorum: quorum,
+		network: networks.regtest,
+		blindingKeypair,
+		tweak,
+	});
 
 	const inputTransactionId = await takeFromFaucet(multisig.confidentialAddress);
 
@@ -116,13 +110,24 @@ async function testMultisigWithParameters(
 
 	const inputTransaction = Transaction.fromHex(inputTransactionHex);
 
+	const tweakedUsedKeysCombination = usedKeysCombination.map((ecpair) => {
+		const tweaker = createKeyTweaker({
+			pubkey: ecpair.publicKey,
+			privkey: ecpair.privateKey,
+		});
+
+		return tweaker.tweakEcpair(tweak);
+	})
+
 	const script = multisig.multisigScripts.find(({ combination }) =>
-		usedKeysCombination.every((ecpair) =>
+		tweakedUsedKeysCombination.every((ecpair) =>
 			combination.includes(ecpair.publicKey.toString("hex"))
 		)
 	);
 
-	expect(script?.leaf.output.toString("hex")).toBe(redeemOutput);
+	expect(script).not.toBeUndefined();
+
+	const redeemOutput = script!.leaf.output.toString("hex");
 
 	const inputTxOutputs = inputTransaction.outs
 		.map((output, vout) => ({ ...output, vout }))
@@ -136,63 +141,83 @@ async function testMultisigWithParameters(
 
 	expect(inputTxOutputs.length).toBe(1);
 
-	const pset = await startSpendFromLiquidMultisig(
-		multisig.hashTree,
+	const pset = await startSpendFromLiquidMultisig({
+		hashTree: multisig.hashTree,
 		redeemOutput,
-		inputTxOutputs.map((output) => ({
+		utxos: inputTxOutputs.map((output) => ({
 			txid: inputTransactionId,
 			hex: inputTransactionHex,
 			vout: output.vout,
 			value: undefined,
 		})),
-		networks.regtest,
-		usedKeysCombination[0]!,
-		[
+		network: networks.regtest,
+		signer: usedKeysCombination[0]!,
+		receivingAddresses: [
 			{
 				address: multisig.confidentialAddress,
-				value: 100_000_000 - 200, // 200 for fees
+				value: 100_000_000 - 200,
 			},
-		]
-	);
+		],
+		blindingKeypair,
+		tweak,
+	});
 
 	if (!pset) throw new Error("Couldn't create pset");
 
 	await Promise.all(
 		usedKeysCombination.slice(1).map(async (keypair) => {
-			await signTaprootTransaction(
+			await signTaprootTransaction({
 				pset,
 				keypair,
-				bip341.tapLeafHash({
+				leafHash: bip341.tapLeafHash({
 					scriptHex: redeemOutput,
 				}),
-				networks.regtest
-			);
+				network: networks.regtest,
+				tweak,
+			});
 		})
 	);
 
-	await finalizeAndPublishTx(
+	await finalizeAndPublishTx({
 		pset,
-		clientKeypairs.map((ecpair) => ecpair.publicKey.toString("hex")),
-		arbitratorKeypairs.map((ecpair) => ecpair.publicKey.toString("hex"))
-	);
+		clients: clientKeypairs.map((ecpair) => ecpair.publicKey.toString("hex")),
+		arbitrators: arbitratorKeypairs.map((ecpair) => ecpair.publicKey.toString("hex")),
+		tweak,
+	});
 }
 
-async function finalizeAndPublishTx(
-	pset: Pset,
-	clients: string[],
-	arbitrators: string[]
-) {
-	const { clientSigs, arbitratorSigs } = getTapscriptSigsOrdered(
-		pset,
-		clients,
-		arbitrators
-	);
+type FinalizeAndPublishTxArgs = {
+	pset: Pset;
+	clients: string[];
+	arbitrators: string[];
+	tweak: Buffer;
+}
 
-	const transaction = finalizeTxSpendingFromLiquidMultisig(
+async function finalizeAndPublishTx({
+	pset,
+	clients,
+	arbitrators,
+	tweak,
+}: FinalizeAndPublishTxArgs) {
+	const { clientSigs, arbitratorSigs } = getTapscriptSigsOrdered({
+		pset,
+		clientPubkeys: clients,
+		arbitratorPubkeys: arbitrators,
+		tweak,
+	});
+
+	expect(clientSigs).not.toSatisfy((e) => {
+		if (!(e instanceof Array))
+			return false;
+
+		return e.every((v) => v === null);
+	}, "client sigs has all values as null");
+
+	const transaction = finalizeTxSpendingFromLiquidMultisig({
 		pset,
 		clientSigs,
-		arbitratorSigs
-	);
+		arbitratorSigs,
+	});
 
 	await publishTransaction(transaction.toHex());
 }
@@ -200,60 +225,63 @@ async function finalizeAndPublishTx(
 describe(
 	"1 arbitrator multisig (connects to local liquid node)",
 	(it) => {
-		const multisigClients = [
-			keypair1.publicKey.toString("hex"),
-			keypair2.publicKey.toString("hex"),
-		];
-		const multisigArbitrators = [keypair3.publicKey.toString("hex")];
+		const multisigClientsKeypairs = new Array(2).fill(null).map(() => ECPair.makeRandom());
 
-		const multisig = createLiquidMultisig(
-			multisigClients,
-			multisigArbitrators,
-			1,
-			networks.regtest
-		);
+		const multisigArbitratorsKeypairs = [ECPair.makeRandom()];
 
-		afterEach(async () => {
-			await sleep(1000);
-		});
+		const blindingKeypair = ECPair.makeRandom();
 
-		it("Can create addresses", () => {
-			expect(multisig.address).toBe(
-				"ert1pcjelkftkp38t74357sq4zwc7wtcg39eglcz657jw2z6ph5u00guqelnywz"
-			);
-			expect(multisig.confidentialAddress).toBe(
-				"el1pqfumuen7l8wthtz45p3ftn58pvrs9xlumvkuu2xet8egzkcklqte339nlvjhvrzwhatrfaqp2ya3uuhs3ztj3ls94fayu595r0fc773cp0mugegr8zz4"
-			);
-		});
+		const dataToTweak = "Some value to tweak";
+
+		const tweak = crypto.sha256(Buffer.from(dataToTweak));
+
+		const [keypair1, keypair2] = (multisigClientsKeypairs as [ECPairInterface, ECPairInterface]);
+
+		const [keypair3] = (multisigArbitratorsKeypairs as [ECPairInterface]);
 
 		it("Can spend with 2 clients", async () => {
-			await testMultisigWithParameters(
-				[keypair1, keypair2],
-				[keypair3],
-				1,
-				"2079be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798ac20c6047f9441ed7d6d3045406e95c07cd85c778e4b8cef3ca7abac09b95c709ee5ba529c",
-				[keypair1, keypair2]
-			);
+			const clientKeypairs = [keypair1, keypair2];
+			const arbitratorKeypairs = [keypair3];
+			const usedKeysCombination = [keypair1, keypair2];
+
+			await testMultisigWithParameters({
+				clientKeypairs,
+				arbitratorKeypairs,
+				quorum: 1,
+				usedKeysCombination,
+				blindingKeypair,
+				tweak,
+			});
 		});
 
 		it("Can spend with client 1 and arbitrator", async () => {
-			await testMultisigWithParameters(
-				[keypair1, keypair2],
-				[keypair3],
-				1,
-				"2079be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798ac20f9308a019258c31049344f85f89d5229b531c845836f99b08601f113bce036f9ba529c",
-				[keypair1, keypair3]
-			);
+			const clientKeypairs = [keypair1, keypair2];
+			const arbitratorKeypairs = [keypair3];
+			const usedKeysCombination = [keypair1, keypair3];
+
+			await testMultisigWithParameters({
+				clientKeypairs,
+				arbitratorKeypairs,
+				quorum: 1,
+				usedKeysCombination,
+				blindingKeypair,
+				tweak,
+			});
 		});
 
 		it("Can spend with client 2 and arbitrator", async () => {
-			await testMultisigWithParameters(
-				[keypair1, keypair2],
-				[keypair3],
-				1,
-				"20c6047f9441ed7d6d3045406e95c07cd85c778e4b8cef3ca7abac09b95c709ee5ac20f9308a019258c31049344f85f89d5229b531c845836f99b08601f113bce036f9ba529c",
-				[keypair3, keypair2]
-			);
+			const clientKeypairs = [keypair1, keypair2];
+			const arbitratorKeypairs = [keypair3];
+			const usedKeysCombination = [keypair3, keypair2];
+
+			await testMultisigWithParameters({
+				clientKeypairs,
+				arbitratorKeypairs,
+				quorum: 1,
+				usedKeysCombination,
+				blindingKeypair,
+				tweak,
+			});
 		});
 	},
 	{
